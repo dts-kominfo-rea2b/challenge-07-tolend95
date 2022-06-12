4d527082468d63c0792feae1947c4f7af9d066ba
Endrial <endrialmp@gmail.com>
2022-06-12 21:32:44 +0700

    first commit

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (affect) => {
+  return Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    .then((res) => {
+      const [iXX, vGC] = res;
+      // return [
+      //   iXX.filter((movie) => movie.hasil === affect).length,
+      //   vGC.filter((movie) => movie.hasil === affect).length,
+      // ];
+      return [...iXX, ...vGC].filter((movie) => movie.hasil === affect).length;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = {
   promiseOutput,
